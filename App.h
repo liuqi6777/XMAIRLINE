2b56d6d7f99e35106f3c2ef3d4edf4d931e407ba
liuqi6777 <3169780038@qq.com>
2021-12-13 20:55:44 +0800

    Add sort functionality.

--- App.h
+++ App.h
@@ -6,10 +6,19 @@
 class App
 {
 public:
-    App() : mgraph(NULL), algraph(NULL) {}
+    App() : mgraph(NULL), algraph(NULL), detail(false), diff(false), max_len(4), departure(-1), arrival(-1) {}
 
     void load_mgraph(MGraph<AirportID, Flights *> *data);
     void load_algraph(ALGraph<AirportID, Flights *> *data);
+
+    void set_detail(bool d) { this->detail = d; }
+    void set_diff(bool d) { this->diff = d; }
+
+    void set_departure_arrival(int d = -1, int a = -1)
+    {
+        this->departure = d;
+        this->arrival = a;
+    }
 
     vector<AirLine> find(AirportID departure, AirportID arrival);
 
@@ -34,17 +43,31 @@ public:
     vector<AirLine> find_by_fare(AirportID departure, AirportID arrival, int least, int most);
     vector<AirLine> find_by_fare(vector<AirLine> airlines, int least, int most);
 
+    void sort_by_fare(vector<AirLine> &airlines);         // 按费用排序
+    void sort_by_airline_time(vector<AirLine> &airlines); // 按飞行时间（含转机时间）排序
+
+    AirLine find_min_by_fare(vector<AirLine> &airlines);
+    AirLine find_min_by_airline_time(vector<AirLine> &airlines);
+
     void run();
 
 private:
     MGraph<AirportID, Flights *> *mgraph;
     ALGraph<AirportID, Flights *> *algraph;
 
+    int departure, arrival;
+    int max_len;
+    bool detail; //输出详细信息
+    bool diff;   // 同一条路线是否输出不同时间
+
+    vector<vector<AirportID>> get_all_routes(const AirportID d, const AirportID a);
     Flights get_flights(const AirportID d, const AirportID a);
     vector<AirLine> flights_to_airlines(const vector<Flights> &flights);
 
     vector<AirLine> find_baisc(AirportID departure, AirportID arrival);
-    void sort_basic(int (*cmp)(const AirLine &, const AirLine &)); // std::sort()
+    void sort_basic(vector<AirLine> &airlines, bool (*cmp)(AirLine &, AirLine &)); // std::sort()
+
+    AirLine find_min(vector<AirLine> &airlines, bool (*cmp)(AirLine &, AirLine &));
 };
 
 void App::run()
@@ -55,28 +78,43 @@ void App::run()
     std::cout << "\nHello, world!\n\n";
 
     cout << "Please input departure airport id and arrival airport id:" << endl;
-    int d, a;
-    cin >> d >> a;
-    vector<AirLine> first_results = find_baisc(d, a);
+    if (departure == -1 && arrival == -1)
+        cin >> departure >> arrival;
 
-    cout << "Follow are all airline(s) (only routes with fewer than 3 transfers are included):" << endl;
-    cout << "(" << first_results.size() << " in total)\n"; 
-    for (int i = 0; i < first_results.size(); i ++)
-        cout << "[AIRLINE " << i+1 << "]" << endl << first_results[i];
+    vector<vector<AirportID>> routes = get_all_routes(departure, arrival);
+    cout << "Follow are all possible route(s) (only routes with fewer than " << max_len - 2
+         << " transfers are included):" << endl;
+    cout << "(" << routes.size() << " in total)" << endl;
+    for (int i = 0; i < routes.size(); i++)
+    {
+        for (int j = 0; j < routes[i].size() - 1; j++)
+            cout << routes[i][j] << " => ";
+        cout << routes[i][routes[i].size() - 1] << endl;
+    }
+    vector<AirLine> first_results = find_baisc(departure, arrival);
 
     cout << "You can give some limits:\n"
-         << "Input 1 to set the range of departure times;\n"
-         << "      2 to set the range of arrival times;\n"
-         << "      3 to choose airplane type;\n"
-         << "      4 to set the range of transit times (0 means direct);\n"
-         << "      5 to set the range of the flight time (including transit time);\n"
-         << "      6 to set the range of fares;\n";
+            "Input 0 means no limits\n"
+            "      1 to set the range of departure times;\n"
+            "      2 to set the range of arrival times;\n"
+            "      3 to choose airplane type;\n"
+            "      4 to set the range of transit times (0 means direct);\n"
+            "      5 to set the range of the flight time (including transit time);\n"
+            "      6 to set the range of fares;\n";
 
-    int m = 0;
+    int m = -1;
     cin >> m;
-    
+
     vector<AirLine> res;
-    if (m == 1)
+    if (m == 0)
+    {
+        res = first_results;
+        // cout << "(" << first_results.size() << " possible airlines in total)" << endl;
+        // for (int i = 0; i < first_results.size(); i++)
+        //     cout << "[AIRLINE " << i + 1 << "]" << endl
+        //          << first_results[i];
+    }
+    else if (m == 1)
     {
         int year, month, day, hour, min;
         cout << "Please input starting time(format: y m d h min): ";
@@ -86,31 +124,20 @@ void App::run()
         cin >> year >> month >> day >> hour >> min;
         Datetime end(year, month, day, hour, min);
         res = find_by_departure_time(first_results, start, end);
-
-        cout << "Follow are all airlines found by departure time:" << endl;
-        cout << "(" << res.size() << " in total)\n"; 
-        for (int i = 0; i < res.size(); i ++)
-            cout << res[i];
     }
     else if (m == 2)
     {
-        int year, month, day, hour, min;
-        cout << "Please input starting time(format: y m d h min): ";
-        cin >> year >> month >> day >> hour >> min;
-        Datetime start(year, month, day, hour, min);
+        string datetime;
+        cout << "Please input starting time(format: month/day/year hour:minute): ";
+        getline(cin, datetime);
+        Datetime start(datetime);
         cout << "Please input ending time: ";
-        cin >> year >> month >> day >> hour >> min;
-        Datetime end(year, month, day, hour, min);
+        getline(cin, datetime);
+        Datetime end(datetime);
         res = find_by_arrival_time(first_results, start, end);
-
-        cout << "Follow are all airlines found by arrival time:" << endl;
-        cout << "(" << res.size() << " in total)\n"; 
-        for (int i = 0; i < res.size(); i ++)
-            cout << res[i];
     }
     else if (m == 3)
     {
-
     }
     else if (m == 4)
     {
@@ -118,11 +145,6 @@ void App::run()
         cout << "Please input the max num of transit times (0 means direct): ";
         cin >> times;
         res = find_by_transit_times(first_results, times);
-
-        cout << "Follow are all airlines found by transit times:" << endl;
-        cout << "(" << res.size() << " in total)\n"; 
-        for (int i = 0; i < res.size(); i ++)
-            cout << res[i];
     }
     else if (m == 5)
     {
@@ -134,11 +156,6 @@ void App::run()
         cin >> day >> hour >> minute;
         TimeDelta most(day, hour, minute);
         res = find_by_airline_time(first_results, least, most);
-
-        cout << "Follow are all airlines found by airline time:" << endl;
-        cout << "(" << res.size() << " in total)\n"; 
-        for (int i = 0; i < res.size(); i ++)
-            cout << res[i];
     }
     else if (m == 6)
     {
@@ -148,16 +165,35 @@ void App::run()
         cout << "Please input the max fares: ";
         cin >> most;
         res = find_by_fare(first_results, least, most);
-
-        cout << "Follow are all airlines found by fares:" << endl;
-        cout << "(" << res.size() << " in total)\n"; 
-        for (int i = 0; i < res.size(); i ++)
-            cout << res[i];
     }
     else
     {
         cout << "Please input a number between 1 and 6!\n";
+        return;
     }
+
+    cout << "You can sort the results by different rules: \n"
+            "Input 0 means no sort; \n"
+            "      1 to sort by fares;\n"
+            "      2 to sort by airline time;\n";
+    int s = 0;
+    cin >> s;
+
+    if (s == 1)
+        sort_by_fare(res);
+    else if (s == 2)
+        sort_by_airline_time(res);
+
+    int n = 10;
+    cout << "Please input a number to determine the number of output routes (if not all output):";
+    cin >> n;
+    n = n < res.size() ? n : res.size();
+
+    cout << "Follow are all airlines found:" << endl
+         << "(" << n << " in total)\n";
+    for (int i = 0; i < n; i++)
+        cout << "[AIRLINE " << i + 1 << "]" << endl
+             << res[i];
 }
 
 void App::load_mgraph(MGraph<AirportID, Flights *> *data)
@@ -168,6 +204,11 @@ void App::load_mgraph(MGraph<AirportID, Flights *> *data)
 void App::load_algraph(ALGraph<AirportID, Flights *> *data)
 {
     this->algraph = data;
+}
+
+vector<vector<AirportID>> App::get_all_routes(const AirportID d, const AirportID a)
+{
+    return mgraph->find_all_paths(d, a, max_len);
 }
 
 Flights App::get_flights(const AirportID d, const AirportID a)
@@ -182,12 +223,12 @@ static void choose1flight(const vector<Flights> &flights, vector<AirLine> &res, 
         res.push_back(AirLine(stack));
         return;
     }
-    for (int j = 0; j < flights[i].size(); j ++)
+    for (int j = 0; j < flights[i].size(); j++)
     {
-        if (i > 0 && flights[i][j].get_departure_time() < stack[i-1].get_arrival_time())
+        if (i > 0 && flights[i][j].get_departure_time() < stack[i - 1].get_arrival_time())
             continue;
         stack.push_back(flights[i][j]);
-        choose1flight(flights, res, stack, i+1);
+        choose1flight(flights, res, stack, i + 1);
         stack.pop_back();
     }
 }
@@ -204,7 +245,7 @@ vector<AirLine> App::flights_to_airlines(const vector<Flights> &flights)
 vector<AirLine> App::find_baisc(AirportID departure, AirportID arrival)
 {
     // 求两个顶点之间所有符合条件的航线
-    vector<vector<AirportID>> all_path = mgraph->find_all_paths(departure, arrival, 3);
+    vector<vector<AirportID>> all_path = get_all_routes(departure, arrival);
     vector<vector<Flights>> all_arcs(all_path.size());
     for (int i = 0; i < all_path.size(); i++)
     {
@@ -314,4 +355,29 @@ vector<AirLine> App::find_by_fare(vector<AirLine> airlines, int least, int most)
 vector<AirLine> App::find_by_fare(AirportID departure, AirportID arrival, int least, int most)
 {
     return find_by_fare(find_baisc(departure, arrival), least, most);
+}
+
+void App::sort_basic(vector<AirLine> &airlines, bool (*cmp)(AirLine &, AirLine &))
+{
+    std::sort(airlines.begin(), airlines.end(), cmp);
+}
+
+static bool cmp_by_fare(AirLine &a1, AirLine &a2)
+{
+    return a1.get_fares() < a2.get_fares();
+}
+
+static bool cmp_by_airline_time(AirLine &a1, AirLine &a2)
+{
+    return a1.get_airline_time() < a2.get_airline_time();
+}
+
+void App::sort_by_fare(vector<AirLine> &airlines)
+{
+    sort_basic(airlines, cmp_by_fare);
+}
+
+void App::sort_by_airline_time(vector<AirLine> &airlines)
+{
+    sort_basic(airlines, cmp_by_airline_time);
 }
